842ca836463bf5ce447317f8867155650b173fa3
Umut Can Arslan <umutcan.arslan1998@hotmail.com>
2022-03-25 15:36:13 +0300

    Create search screen ui

--- GithubApp/GithubApp/Scenes/Search/SearchViewController.swift
+++ GithubApp/GithubApp/Scenes/Search/SearchViewController.swift
@@ -8,10 +8,63 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    let logoImageView = UIImageView()
+    let usernameTextField = GHTextField()
+    let callToActionButton = GHButton(backgroundColor: .systemGreen, title: "Get Followers")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+        configureTextField()
+        configureCTAButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate(
+            [
+                logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+                logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                logoImageView.heightAnchor.constraint(equalToConstant: 200),
+                logoImageView.widthAnchor.constraint(equalToConstant: 200)
+            ]
+        )
+    }
+    
+    func configureTextField() {
+        view.addSubview(usernameTextField)
+        
+        NSLayoutConstraint.activate(
+            [
+                usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+                usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+                usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+                usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+            ]
+        )
+    }
+    
+    func configureCTAButton() {
+        view.addSubview(callToActionButton)
+        
+        NSLayoutConstraint.activate(
+            [
+                callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+                callToActionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
+                callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+                callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+            ]
+        )
     }
     
 }
---
+++ GithubApp/GithubApp/UICompanents/GHButton.swift
@@ -0,0 +1,36 @@
+//
+//  GithubButton.swift
+//  GithubApp
+//
+//  Created by Umut Can Arslan on 25.03.2022.
+//
+
+import UIKit
+
+class GHButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.headline)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+}
