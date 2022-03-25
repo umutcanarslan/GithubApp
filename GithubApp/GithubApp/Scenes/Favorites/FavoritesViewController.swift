744bb7f81825678a3629f8ec8578e501994338d2
Umut Can Arslan <umutcan.arslan1998@hotmail.com>
2022-03-25 11:15:01 +0300

    Create tabbar and navigationbar

---
+++ GithubApp/GithubApp/Scenes/Favorites/FavoritesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesViewController.swift
+//  GithubApp
+//
+//  Created by Umut Can Arslan on 25.03.2022.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+    
+}
---
+++ GithubApp/GithubApp/Scenes/Search/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  GithubApp
+//
+//  Created by Umut Can Arslan on 25.03.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+    
+}
