5f8629723d8f971570a1c66ec97f03d9f97883af
Umut Can Arslan <umutcan.arslan1998@hotmail.com>
2022-03-27 16:49:40 +0300

    Add text field control and push follower list screen

--- GithubApp/GithubApp/Scenes/Search/SearchViewController.swift
+++ GithubApp/GithubApp/Scenes/Search/SearchViewController.swift
@@ -9,62 +9,85 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
-    let logoImageView = UIImageView()
-    let usernameTextField = GHTextField()
-    let callToActionButton = GHButton(backgroundColor: .systemGreen, title: "Get Followers")
-
+    private let logoImageView = UIImageView()
+    private let usernameTextField = GHTextField()
+    private let callToActionButton = GHButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    var isUsernameEntered: Bool {
+        return !usernameTextField.text!.isEmpty
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        configureLogoImageView()
-        configureTextField()
-        configureCTAButton()
+        setupUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = true
+        usernameTextField.becomeFirstResponder()
     }
     
-    func configureLogoImageView() {
+    @objc private func dismissKeyboard() {
+        usernameTextField.resignFirstResponder()
+    }
+    
+}
+
+//  MARK: UI Configure
+extension SearchViewController {
+    
+    private func setupUI() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
         view.addSubview(logoImageView)
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
         logoImageView.image = UIImage(named: "gh-logo")!
+        
+        view.addSubview(usernameTextField)
+        usernameTextField.delegate = self
+        
+        view.addSubview(callToActionButton)
+        callToActionButton.addTarget(self, action: #selector(pushFollowerListViewController), for: .touchUpInside)
         
         NSLayoutConstraint.activate(
             [
                 logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
                 logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
                 logoImageView.heightAnchor.constraint(equalToConstant: 200),
-                logoImageView.widthAnchor.constraint(equalToConstant: 200)
-            ]
-        )
-    }
-    
-    func configureTextField() {
-        view.addSubview(usernameTextField)
-        
-        NSLayoutConstraint.activate(
-            [
+                logoImageView.widthAnchor.constraint(equalToConstant: 200),
+                
                 usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
                 usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
                 usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-                usernameTextField.heightAnchor.constraint(equalToConstant: 50)
-            ]
-        )
-    }
-    
-    func configureCTAButton() {
-        view.addSubview(callToActionButton)
-        
-        NSLayoutConstraint.activate(
-            [
+                usernameTextField.heightAnchor.constraint(equalToConstant: 50),
+                
                 callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
                 callToActionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
                 callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
                 callToActionButton.heightAnchor.constraint(equalToConstant: 50)
             ]
         )
+    }
+    
+}
+
+//  MARK: Delegates
+extension SearchViewController: UITextFieldDelegate {
+    
+    @objc func pushFollowerListViewController() {
+        guard isUsernameEntered else { return }
+        let followerListViewController = FollowerListViewController()
+        followerListViewController.username = usernameTextField.text
+        followerListViewController.title = usernameTextField.text
+        navigationController?.pushViewController(followerListViewController, animated: true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        pushFollowerListViewController()
+        return true
     }
     
 }
