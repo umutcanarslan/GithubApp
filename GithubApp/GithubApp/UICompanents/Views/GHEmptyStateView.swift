bc001b005abd4521f33fc211d1f79991644356e4
Umut Can Arslan <umutcan.arslan1998@hotmail.com>
2022-03-28 22:46:20 +0300

    Refactor directory style

---
+++ GithubApp/GithubApp/UICompanents/Views/GHEmptyStateView.swift
@@ -0,0 +1,20 @@
+//
+//  GHEmptyStateView.swift
+//  GithubApp
+//
+//  Created by Umut Can Arslan on 28.03.2022.
+//
+
+import UIKit
+
+class GHEmptyStateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
