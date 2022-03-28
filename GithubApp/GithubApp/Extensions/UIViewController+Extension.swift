4823959206bb5ed4b10dd83cf71fb02fcb8be914
Umut Can Arslan <umutcan.arslan1998@hotmail.com>
2022-03-28 22:31:55 +0300

    Add activity indicator on followers list screen

--- GithubApp/GithubApp/Extensions/UIViewController+Extension.swift
+++ GithubApp/GithubApp/Extensions/UIViewController+Extension.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
     
     func presentAlertOnMainThread(alertHeader: String, alertMessage: String, buttonText: String) {
@@ -19,6 +21,36 @@ extension UIViewController {
             alertViewController.modalPresentationStyle = .overFullScreen
             alertViewController.modalTransitionStyle = .crossDissolve
             self.present(alertViewController, animated: true)
+        }
+    }
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8 }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(
+            [
+                activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ]
+        )
+        activityIndicator.startAnimating()
+    }
+    
+    func hideLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
     
--- GithubApp/GithubApp/Scenes/FollowerList/FollowerListViewController.swift
+++ GithubApp/GithubApp/Scenes/FollowerList/FollowerListViewController.swift
@@ -11,8 +11,10 @@ class FollowerListViewController: UIViewController {
     
     enum Section { case main }
     
-    var followers: [Follower] = []
     var username: String!
+    var followers: [Follower] = []
+    var page = 1
+    var hasMoreFollowers = true
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -21,7 +23,7 @@ class FollowerListViewController: UIViewController {
         super.viewDidLoad()
         setupUI()
         setupCollectionView()
-        getFollowers()
+        getFollowers(username: username, page: page)
         configureDataSource()
     }
     
@@ -35,20 +37,21 @@ class FollowerListViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
-    private func getFollowers() {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+    func getFollowers(username: String, page: Int) {
+        showLoadingView()
+        NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
-            
+            self.hideLoadingView()
             switch result {
             case .success(let followers):
-                self.followers = followers
+                if followers.count < 100 { self.hasMoreFollowers = false }
+                self.followers.append(contentsOf: followers)
                 self.updateData()
             case .failure(let error):
                 self.presentAlertOnMainThread(
                     alertHeader: "Bad Stuff Happend",
                     alertMessage: error.rawValue,
-                    buttonText: "OK"
-                )
+                    buttonText: "Okey")
             }
         }
     }
@@ -61,6 +64,7 @@ extension FollowerListViewController {
     private func setupCollectionView() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.configureCollectionView(in: view))
         view.addSubview(collectionView)
+        collectionView.delegate = self
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCollectionViewCell.self, forCellWithReuseIdentifier: FollowerCollectionViewCell.identifier)
     }
@@ -86,6 +90,22 @@ extension FollowerListViewController {
         
         DispatchQueue.main.async {
             self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+    }
+    
+}
+
+extension FollowerListViewController: UICollectionViewDelegate {
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let offsetY         = scrollView.contentOffset.y
+        let contentHeight   = scrollView.contentSize.height
+        let height          = scrollView.frame.size.height
+        
+        if offsetY > (contentHeight - height) {
+            guard hasMoreFollowers else { return }
+            page += 1
+            self.getFollowers(username: username, page: page)
         }
     }
     
