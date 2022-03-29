1d0907e0c3eff2b52a563fafccc0d6f511907ffd
Umut Can Arslan <umutcan.arslan1998@hotmail.com>
2022-03-29 21:10:43 +0300

    Add user info fetching

--- GithubApp/GithubApp/Model/User.swift
+++ GithubApp/GithubApp/Model/User.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 struct User: Codable {
-    var login: String
-    var avatarUrl: String
-    var htmlUrl: String
+    let login: String
+    let avatarUrl: String
     var name: String?
     var location: String?
-    var biography: String?
-    var publicRepo: Int
-    var publicGists: Int
-    var following: Int
-    var foloowers: Int
-    var createdAt: String
+    var bio: String?
+    let publicRepos: Int
+    let publicGists: Int
+    let htmlUrl: String
+    let following: Int
+    let followers: Int
+    let createdAt: String
 }
