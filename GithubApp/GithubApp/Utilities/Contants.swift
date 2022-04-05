ad1388e67e94dd1eea2da0577e1272cf0ea950bc
Umut Can Arslan <umutcan.arslan1998@hotmail.com>
2022-04-05 09:48:10 +0300

    Create contants items

---
+++ GithubApp/GithubApp/Utilities/Contants.swift
@@ -0,0 +1,16 @@
+//
+//  Contants.swift
+//  GithubApp
+//
+//  Created by Umut Can Arslan on 29.03.2022.
+//
+
+import Foundation
+
+enum SFSymbol {
+    static let location = "mappin.circle"
+    static let repositories = "book.closed.fill"
+    static let gists = "doc.text.fill"
+    static let follower = "heart.fill"
+    static let following = "person.2.fill"
+}
