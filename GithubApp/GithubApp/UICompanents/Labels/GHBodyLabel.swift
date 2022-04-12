a2b8881fcd4976df1eb71037eea066b3b2a9b87e
Umut Can Arslan <umutcan.arslan1998@hotmail.com>
2022-04-12 08:16:20 +0300

    Add dynamic type on body label

--- GithubApp/GithubApp/UICompanents/Labels/GHBodyLabel.swift
+++ GithubApp/GithubApp/UICompanents/Labels/GHBodyLabel.swift
@@ -26,6 +26,7 @@ class GHBodyLabel: UILabel {
     private func configure() {
         textColor = .secondaryLabel
         font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontForContentSizeCategory = true
         adjustsFontSizeToFitWidth = true
         minimumScaleFactor = 0.75
         lineBreakMode = .byWordWrapping
